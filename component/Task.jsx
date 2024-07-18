bc4336fc7e950b9cb2f30eacd6996d6aa834f161
Chanawee <Chanawee_P@cmu.ac.th>
2024-07-18 14:45:04 +0700

    Done_again

--- component/Task.jsx
+++ component/Task.jsx
@@ -1,9 +1,17 @@
 export const Task = (props) => {
-    return (
+  if(props.isdone == true){
+    <div className="d-flex p-3 gap-2 align-items-center border-bottom text-decoration-line-through">
+        <span>{props.title}</span>
+        <button className="btn btn-success">Done</button>
+        <button className="btn btn-danger">Delete</button>
+      </div>
+  }  
+  else{return (
         <div className="d-flex p-3 gap-2 align-items-center border-bottom">
         <span>{props.title}</span>
         <button className="btn btn-success">Done</button>
         <button className="btn btn-danger">Delete</button>
       </div>
     );
+  }
 };
